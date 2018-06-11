f8b4f301a22b64c57a6bf973301908751f328681
Stian Westvig <stian.westvig@bouvet.no>
2018-06-11 14:49:16 +0200

    Drawing three themed ninjas

--- src/app.js
+++ src/app.js
@@ -26,14 +26,53 @@ function drawLine(context, pixels) {
   );
 }
 
-function pixel(context, x, y) {
-  drawScaled(context, x, y)
-}
-
 const canvas = init();
 const c = canvas.getContext('2d');
 
-const rowContext = { c, xOffset: 60, yOffset: 60, scale: 10, row: 0 };
-drawLine(rowContext, ['INDIANRED', 'LIGHTCORAL', 'LIGHTCORAL', 'SALMON', 'DARKSALMON']);
-drawLine({ ...rowContext, row: 1 }, ['INDIANRED', 'BLACK', 'LIGHTCORAL', 'BLACK', 'DARKSALMON']);
-drawLine({ ...rowContext, row: 2 }, ['INDIANRED', 'LIGHTCORAL', 'LIGHTCORAL', 'SALMON', 'DARKSALMON']);
+function drawNinja(figure) {
+  const theme = figure.theme;
+  drawLine({ ...figure, row: 0 }, ['transparent', 'transparent', 'transparent', theme.main, theme.main, 'transparent', 'transparent', 'transparent']);
+  drawLine({ ...figure, row: 1 }, ['transparent', theme.main, theme.main, theme.main, theme.main, theme.main, theme.main, 'transparent']);
+  drawLine({ ...figure, row: 2 }, ['transparent', theme.main, theme.skin, 'black', theme.skin, 'black', theme.main, 'transparent']);
+  drawLine({ ...figure, row: 3 }, [theme.main, theme.main, theme.main, theme.main, theme.main, theme.main, theme.main, theme.main]);
+  drawLine({ ...figure, row: 4 }, ['transparent', theme.main, theme.main, theme.main, theme.main, theme.main, theme.main, 'transparent']);
+  drawLine({ ...figure, row: 5 }, ['transparent', theme.main, theme.main, theme.main, theme.main, theme.main, theme.main, 'transparent']);
+  drawLine({ ...figure, row: 6 }, ['transparent', 'transparent', theme.main, 'transparent', 'transparent', theme.main, 'transparent', 'transparent']);
+  drawLine({ ...figure, row: 7 }, ['transparent', 'transparent', theme.main, 'transparent', 'transparent', theme.main, 'transparent', 'transparent']);
+}
+
+const ninjaBlue = {
+  c,
+  xOffset: 60,
+  yOffset: 60,
+  scale: 10,
+  theme: {
+    main: '#92DCE5',
+    skin: '#FFA07A'
+  }
+};
+drawNinja(ninjaBlue);
+
+const ninjaDark = {
+  c,
+  xOffset: 160,
+  yOffset: 60,
+  scale: 10,
+  theme: {
+    main: '#2B303A',
+    skin: '#FFA07A'
+  }
+};
+drawNinja(ninjaDark);
+
+const ninjaRed = {
+  c,
+  xOffset: 260,
+  yOffset: 60,
+  scale: 10,
+  theme: {
+    main: '#D64933',
+    skin: '#FFA07A'
+  }
+};
+drawNinja(ninjaRed);
