cd55ef78ad286e7ff6466e12abcd91693e067cc6
Stian Westvig <stian.westvig@bouvet.no>
2018-08-27 09:04:49 +0200

    Add a separate sprite template

---
+++ src/sprites/template.js
@@ -0,0 +1,22 @@
+
+export const theme = new Map([
+  [0, 'transparent'],
+  [1, '#643173'], // purple
+  [2, '#B4DC7F'], // green
+  [3, '#FADF7F'], // yellow
+]);
+
+const template = [
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+];
+
+export const spriteSheet = new Map();
+spriteSheet.set('template', [flower]);
